54853d7f714adacf25118c2688aa9a50cf4efe6f
Rasmus Nielsen <rasmus@easify.dk>
2017-12-10 23:19:54 +0800

    Apply fixes from StyleCI (#3)

--- database/migrations/2017_08_21_112801_create_escrow_transactions_table.php
+++ database/migrations/2017_08_21_112801_create_escrow_transactions_table.php
@@ -14,18 +14,18 @@ class CreateEscrowTransactionsTable extends Migration
     {
         Schema::create('escrow_transactions', function (Blueprint $table) {
             $table->increments('id');
-            $table->string("source_type");
-            $table->string("source_id");
-            $table->string("destination_type");
-            $table->string("destination_id");
+            $table->string('source_type');
+            $table->string('source_id');
+            $table->string('destination_type');
+            $table->string('destination_id');
             $table->decimal('amount');
             $table->string('currency_code');
             $table->integer('associated_escrow_id')->nullable()->unsigned();
             $table->timestamps();
 
             $table->foreign('associated_escrow_id')->references('id')->on('escrows')->onDelete('set null')->onUpdate('cascade');
-            $table->index(["source_id", "source_type"]);
-            $table->index(["destination_id", "destination_type"]);
+            $table->index(['source_id', 'source_type']);
+            $table->index(['destination_id', 'destination_type']);
         });
     }
 
